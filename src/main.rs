7d01780a5282ea13a6ed843d0cf9672ac8dc3f59
97mams <97mams@gmail.com>
2026-02-14 09:04:25 +0300

    first exercice

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use std::env;
+fn main() { 
+    let name:&str = "Mamisoa";
+    let age:i32 = 28;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    // if args.len() > 2 {
-    //     println!("Usage: cargo run -- <name>");
-    //     return;
-    // } 
-    let name = &args[1];
-    let age = &args[2];
-    println!("Hello, {}! You are {} years old.", name, age);
+    println!("Bonjour {name}, tu as {age} ans");
 }
