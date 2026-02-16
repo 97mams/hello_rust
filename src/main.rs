db9925a7c3e1e018fdf489465d6baad14b789427
97mams <97mams@gmail.com>
2026-02-16 08:33:57 +0300

    refactor main function to use User struct and update first_name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
-fn main() { 
-    let name:&str = "Mamisoa";
-    let age:i32 = 28;
 
-    println!("Bonjour {name}, tu as {age} ans");
+//first ex 
+
+// fn main() { 
+//     let name:&str = "Mamisoa";
+//     let age:i32 = 28;
+
+//     println!("Bonjour {name}, tu as {age} ans");
+// }
+#[derive(Debug)]
+struct User {
+    first_name: String,
+    last_name: String,
+    age: u8
+}
+
+fn main () {
+    let mut mams = User {
+        first_name: String::from("Mamisoa"),
+        last_name: String::from("Ratsimbarison"),
+        age: 28 
+    };
+
+    mams.first_name = String::from("Santatra");
+
+    println!("{:?}", mams);
 }
