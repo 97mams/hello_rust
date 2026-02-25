b5c7a2e88b9aaa2fbf7687d91b698c76f69470cf
97mams <97mams@gmail.com>
2026-02-25 11:40:33 +0300

    refactor main function to implement double calculation and user input

--- src/main.rs
+++ src/main.rs
@@ -89,16 +89,59 @@
 //     squirtle.attack();
 //     bulbasaur.attack();
 // }
-mod exo2;
+// mod exo2;
+
+// fn main() {
+//     let mut tasks: Vec<exo2::Task> = Vec::new();
+
+//     tasks.push(exo2::Task::new(1, String::from("Faire les courses")));
+//     tasks.push(exo2::Task::new(2, String::from("Nettoyer la maison")));
+//     tasks.push(exo2::Task::new(3, String::from("Faire du sport")));
+
+//     tasks[0].is_completed();
+
+//     exo2::Task::list_tasks(&tasks);
+// }
+// use std::collections::HashMap;
+
+// fn main() {
+//     // let mut scores = HashMap::new();
+//     // scores.insert(String::from("FTA"), 0);
+//     // scores.insert(String::from("2930"), 2);
+//     let equipes: Vec<String> = vec![String::from("FTA"), String::from("2930")];
+//     let scores: Vec<u32> = vec![0, 2];
+//     let scores: HashMap<_, _> = equipes.into_iter().zip(scores.into_iter().map(|s| s as u32)).collect();
+
+//     print!("{:?}", scores);
+//     print!("Le score de FTA est de {}", scores.get("FTA").unwrap());
+//     print!("Le score de 2930 est de {}", scores.get("2930").unwrap());
+
+// }
+ 
+use std::io;
+
+fn double(n: i64, multiplier: f64) -> f64 {
+    let mut init_value:f64 = 5000.0;
+    let mut result = init_value;
+    for _ in 0..n {
+        result = init_value * multiplier;
+        init_value = result;
+
+    }
+    result
+}
 
 fn main() {
-    let mut tasks: Vec<exo2::Task> = Vec::new();
-
-    tasks.push(exo2::Task::new(1, String::from("Faire les courses")));
-    tasks.push(exo2::Task::new(2, String::from("Nettoyer la maison")));
-    tasks.push(exo2::Task::new(3, String::from("Faire du sport")));
-
-    tasks[0].is_completed();
-
-    exo2::Task::list_tasks(&tasks);
+    println!("Entrez un nombre :");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let n: i64 = input.trim().parse().expect("Please enter a valid number");
+    println!("Entrez un multiplicateur :");
+    let mut input2 = String::new();
+    io::stdin().read_line(&mut input2).expect("Failed to read line");
+    let multiplier: f64 = input2.trim().parse().expect("Please enter a valid number");
+    let result = double(n, multiplier);
+    println!("Le résultat de {} multiplié par {} est {}", n, multiplier, result);
 }
+
+
